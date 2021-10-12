86c3333920299b9729528cd830029d9f59ce2a58
Protuberanets <k.kasterov@gmail.com>
2021-10-12 11:50:31 +0300

    Homework 1

---
+++ src/main/java/ru/gb/kasterov/HomeWorkApp.java
@@ -0,0 +1,47 @@
+package ru.gb.kasterov;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -30, b = 10;
+        int summ = a + b;
+        if (summ >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+        System.out.println("Сумма = " + summ);
+       }
+
+    public static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10, b = -342;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
